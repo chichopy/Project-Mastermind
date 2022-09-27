448ac6f2ede19c968e068db834437c82e49a0a91
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-27 11:17:46 -0400

    Change feedback from array of values to array of dicts
    This change allow users to read feadback much easier than previous implementation

--- app.rb
+++ app.rb
@@ -9,7 +9,9 @@ class MasterMind
     @pc_chosen_colors = []
     @aux_pc_chosen_colors = []
     @user_chosen_colors = []
+    @aux_user_chosen_colors = []
     @guess = []
+    @dic_color = { Color: '', Exists: false, Position: 'Incorrect' }
   end
 
   def pc_choices
@@ -30,34 +32,70 @@ class MasterMind
       p @user_chosen_colors
       n += 1
     end
+    @user_chosen_colors.each { |value| @aux_user_chosen_colors.push(value) }
   end
 
   def round
     @user_chosen_colors.each_with_index do |color, i|
+      next unless color == @pc_chosen_colors[i]
+
+      @dic_color[:Color] = color
+      @dic_color[:Exists] = true
+      @dic_color[:Position] = "#{i + 1} Correct"
+      @guess.push(@dic_color)
+      @dic_color = { Color: '', Exists: false, Position: 'Incorrect' }
+      @pc_chosen_colors.delete_at(@pc_chosen_colors.find_index(color))
+      @user_chosen_colors.delete_at(@user_chosen_colors.find_index(color))
+    end
+    p 'Before enter user that iterates'
+    p @user_chosen_colors
+    @user_chosen_colors.each do |color|
+      @dic_color[:Color] = color
       if @pc_chosen_colors.include?(color)
-        comparison = @user_chosen_colors[i] == @aux_pc_chosen_colors[i]
-        comparison ? @guess.push("Position & Color: #{color} Correct") : @guess.push("Only Color: #{color} Correct")
+        @dic_color[:Exists] = true
+        @guess.push(@dic_color)
+        @dic_color = { Color: '', Exists: false, Position: 'Incorrect' }
         @pc_chosen_colors.delete_at(@pc_chosen_colors.find_index(color))
       else
-        @guess.push("Color: #{color} Incorrect")
+        @guess.push(@dic_color)
+        @dic_color = { Color: '', Exists: false, Position: 'Incorrect' }
       end
     end
-    @pc_chosen_colors = @aux_pc_chosen_colors
+    @pc_chosen_colors = []
+    @user_chosen_colors = []
+    @aux_pc_chosen_colors.each { |value| @pc_chosen_colors.push(value) }
+    @aux_user_chosen_colors.each { |value| @user_chosen_colors.push(value) }
+  end
+
+  def game
+    pc_choices
+    user_win = false
+    12.times do
+      user_choice
+      round
+      if @user_chosen_colors == @pc_chosen_colors
+        puts 'You Win!'
+        user_win = true
+        break
+      else
+        puts
+        puts
+        puts
+        print 'user: '
+        p @user_chosen_colors
+        puts
+        print 'pc: '
+        p @pc_chosen_colors
+        puts
+        puts "Guess: \n\n#{@guess} \n\n Try again\n\n"
+        @guess = []
+        @user_chosen_colors = []
+        @aux_user_chosen_colors = []
+      end
+    end
+    puts 'You lost!' unless user_win
   end
 end
 
 matrix = MasterMind.new
-matrix.pc_choices
-matrix.user_choice
-matrix.round
-# puts
-# puts
-# puts
-# print 'user: '
-# p matrix.user_chosen_colors
-# puts
-# print 'pc: '
-# p matrix.pc_chosen_colors
-# puts
-# print 'guess: '
-# p matrix.guess
+matrix.game
